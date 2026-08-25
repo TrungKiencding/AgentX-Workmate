735c2a01dd6f99b46f48b592242635aaa74dc970
Hukla <129692708+huklaa@users.noreply.github.com>
2026-08-25 12:21:17 +0300

    fix(desktop): ignore IME Enter in Kanban task title
    (cherry picked from commit 19729c6ea792fcf733482b584d04c09d0bb9056d)

--- apps/desktop/src/plugins/kanban/board.tsx
+++ apps/desktop/src/plugins/kanban/board.tsx
@@ -78,6 +78,7 @@ import {
 } from './api'
 import { BoardSwitcher } from './board-switcher'
 import { TaskDrawer } from './drawer'
+import { shouldSubmitOnEnter } from './ime-enter'
 import { EMPTY_OVERRIDE, ModelOverrideField, overrideCreateFields, type TaskModelOverride } from './model-override'
 import { OrchestrationPanel } from './orchestration'
 import { columnMeta, type KanbanBoard, type KanbanTask, type TaskEstimate } from './types'
@@ -682,7 +683,7 @@ function NewTaskDialog({
             autoFocus
             onChange={event => setTitle(event.target.value)}
             onKeyDown={event => {
-              if (event.key === 'Enter') {
+              if (shouldSubmitOnEnter(event)) {
                 event.preventDefault()
                 void submit()
               }
