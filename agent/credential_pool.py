NousResearch/hermes-agent

fix(auth): close Anthropic OAuth CSRF gap, cross-process refresh race, and API-key shadowing
Dashboard PKCE login reused the code_verifier as the OAuth state (leaking
it and disabling CSRF validation) and never checked state on callback --
the same class of bug already fixed for the CLI flow. Credential-pool
refresh excluded "anthropic" from the cross-process lock Codex/xAI already
get, so concurrent Hermes processes racing a single-use refresh token could
leave the loser stuck exhausted with no recovery for hermes_pkce/dashboard
sources. The dashboard OAuth save also never cleared a stale
ANTHROPIC_API_KEY, which resolve_anthropic_token() prioritizes over the
OAuth pool entry by design -- so a leftover key silently kept billing
pay-per-token after a Claude Pro/Max login.

A concurrency stress test written to validate the refresh-race fix under
load surfaced a fifth, unrelated bug: _auth_store_lock()'s Windows
lock-file "ensure content" write was unguarded and could raise an uncaught
PermissionError under real contention -- affecting every single-use-token
provider sharing that lock, not just Anthropic.

Fixes #87887, #87888, #87889.

agent/credential_pool.py
@@ -904,6 +904,49 @@ class CredentialPool:
             logger.debug("Failed to sync from credentials file: %s", exc)
         return entry
 
+    def _sync_anthropic_entry_from_pool_store(
+        self, entry: PooledCredential
+    ) -> PooledCredential:
+        """Adopt an Anthropic token pair rotated by another pool instance.
+
+        Unlike ``_sync_anthropic_entry_from_credentials_file`` (which only
+        helps ``entry.source == "claude_code"`` by re-reading
+        ``~/.claude/.credentials.json``), this re-reads the exact persisted
+        row from the credential-pool store itself
+        (``~/.hermes/auth.json`` / profile equivalent), so it works for
+        every Anthropic source — ``claude_code``, ``hermes_pkce``, and
+        dashboard-issued ``manual:dashboard_pkce`` entries alike. Called
+        while the shared cross-process auth-store lock is held, mirroring
+        ``_sync_xai_oauth_entry_from_pool_store``.
+        """
+        if self.provider != "anthropic":
+            return entry
+        try:
+            persisted = next(
+                (
+                    payload
+                    for payload in read_credential_pool(self.provider)
+                    if isinstance(payload, dict) and payload.get("id") == entry.id
+                ),
+                None,
+            )
+            if not isinstance(persisted, dict):
+                return entry
+            stored = PooledCredential.from_dict(self.provider, persisted)
+            if (
+                stored.access_token != entry.access_token
+                or stored.refresh_token != entry.refresh_token
+            ):
+                logger.debug(
+                    "Pool entry %s: adopting Anthropic OAuth tokens rotated by another pool instance",
+                    entry.id,
+                )
+                self._replace_entry(entry, stored)
+                return stored
+        except Exception as exc:
+            logger.debug("Failed to sync Anthropic OAuth entry from credential pool: %s", exc)
+        return entry
+
     def _sync_codex_entry_from_auth_store(self, entry: PooledCredential) -> PooledCredential:
         """Sync a Codex device_code pool entry from auth.json if tokens differ.
 
@@ -1319,11 +1362,22 @@ class CredentialPool:
         # resolve_codex_runtime_credentials()).  When a waiter finally acquires
         # the lock, the in-lock re-sync below picks up the rotated token the
         # winner persisted and skips the POST.
-        if self.provider in ("openai-codex", "xai-oauth"):
+        # Anthropic's OAuth refresh tokens are single-use too (see
+        # agent/anthropic_adapter.py::_refresh_oauth_token), so the same
+        # cross-process serialization Codex/xAI get is required here.
+        # Previously "anthropic" was excluded from this tuple: two Hermes
+        # processes racing to refresh the same stale token would both POST,
+        # the loser got invalid_grant, and — for any source other than
+        # "claude_code" (hermes_pkce, dashboard-issued manual entries) —
+        # there was no recovery path at all, so the loser was marked
+        # exhausted despite a valid token existing on disk from the winner.
+        if self.provider in ("openai-codex", "xai-oauth", "anthropic"):
             sync_entry = (
                 self._sync_codex_entry_from_auth_store
                 if self.provider == "openai-codex"
                 else self._sync_xai_oauth_entry_from_pool_store
+                if self.provider == "xai-oauth"
+                else self._sync_anthropic_entry_from_pool_store
             )
             with _auth_store_lock(
                 timeout_seconds=self._single_use_refresh_lock_timeout()
@@ -1355,6 +1409,8 @@ class CredentialPool:
             "HERMES_CODEX_REFRESH_TIMEOUT_SECONDS"
             if self.provider == "openai-codex"
             else "HERMES_XAI_REFRESH_TIMEOUT_SECONDS"
+            if self.provider == "xai-oauth"
+            else "HERMES_ANTHROPIC_REFRESH_TIMEOUT_SECONDS"
         )
         refresh_timeout_seconds = auth_mod.env_float(env_var, 20)
         return max(
@@ -1481,6 +1537,31 @@ class CredentialPool:
                     # Credentials file had a valid (non-expired) token — use it directly
                     logger.debug("Credentials file has valid token, using without refresh")
                     return synced
+            elif self.provider == "anthropic":
+                # Backstop for non-claude_code sources (hermes_pkce,
+                # manual:dashboard_pkce): the in-lock pre-check in
+                # _refresh_entry() should already have adopted a winner's
+                # rotated token before this POST was even attempted, but if
+                # the failure still happened (e.g. the winner persisted
+                # between our pre-check and our POST), re-read the pool
+                # store once more before giving up.
+                synced = self._sync_anthropic_entry_from_pool_store(entry)
+                if synced.refresh_token != entry.refresh_token:
+                    logger.debug(
+                        "Anthropic OAuth refresh failed but pool store has newer tokens — adopting"
+                    )
+                    updated = replace(
+                        synced,
+                        last_status=STATUS_OK,
+                        last_status_at=None,
+                        last_error_code=None,
+                        last_error_reason=None,
+                        last_error_message=None,
+                        last_error_reset_at=None,
+                    )
+                    self._replace_entry(synced, updated)
+                    self._persist()
+                    return updated
             # For xai-oauth: same race as nous — another process may have
             # consumed the refresh token between our proactive sync and the
             # HTTP call.  Re-check auth.json and adopt the fresh tokens if

tests/hermes_cli/test_auth_store_lock_concurrent.py
@@ -1,0 +1,79 @@
+"""Regression test for a Windows-only PermissionError under lock contention.
+
+``_file_lock()`` (backing ``_auth_store_lock()``) ensures its lock file has
+at least 1 byte of content before opening it for ``msvcrt.locking()`` (which
+requires a non-empty file). That "ensure content" write was unguarded: under
+real concurrency, one thread/process's ``msvcrt.locking()`` byte-range lock
+can already be held on the file at the exact moment another thread runs the
+same ensure-content check, and the write collides and raises
+``PermissionError`` -- uncaught, since it happens before the retry loop the
+rest of the function uses for exactly this kind of contention.
+
+This was found by a stress test for the Anthropic OAuth credential-pool fix
+(``tests/agent/test_anthropic_oauth_stress.py``), which reliably reproduces
+it (16/20 concurrent refreshes hit PermissionError pre-fix, deterministically
+across repeated runs) because the surrounding CredentialPool work widens the
+race window enough for real OS-level thread interleaving to land on it. That
+test is the authoritative regression guard for the exact failure mode. This
+file is deliberately a minimal, isolated stress test against
+``_auth_store_lock()`` directly -- best-effort coverage that does not always
+land on the same narrow window in isolation, but exercises the SAME shared
+primitive Codex/xAI/Nous already depend on for their single-use-refresh-token
+protection, so this bug was never Anthropic-specific.
+"""
+
+from __future__ import annotations
+
+import threading
+
+import pytest
+
+from hermes_cli.auth import _auth_store_lock
+
+CONCURRENCY = 40
+
+
+@pytest.fixture
+def hermes_home(tmp_path, monkeypatch):
+    monkeypatch.setenv("HERMES_HOME", str(tmp_path))
+    return tmp_path
+
+
+def test_many_concurrent_lock_acquisitions_do_not_raise_permission_error(hermes_home):
+    """CONCURRENCY threads race to acquire/release the same auth-store lock.
+
+    None of them should ever see an uncaught PermissionError/OSError from
+    the lock-file "ensure content" pre-check -- only the intended
+    TimeoutError (never expected here, since each critical section is
+    instant) should be a possible failure mode.
+    """
+    errors: dict[int, BaseException] = {}
+    entered = 0
+    entered_lock = threading.Lock()
+    # Synchronize all threads to hit the lock-file "ensure content" pre-check
+    # at (as close as the OS scheduler allows to) the exact same instant --
+    # the race only exists on the very first acquisition against a fresh
+    # lock file, so a tight barrier maximizes the odds of reproducing it
+    # instead of relying on incidental thread-start jitter.
+    barrier = threading.Barrier(CONCURRENCY)
+
+    def _run(idx: int) -> None:
+        nonlocal entered
+        try:
+            barrier.wait(timeout=10)
+            with _auth_store_lock(timeout_seconds=10.0):
+                with entered_lock:
+                    entered += 1
+        except BaseException as exc:  # pragma: no cover - failure diagnostics
+            errors[idx] = exc
+
+    threads = [threading.Thread(target=_run, args=(i,)) for i in range(CONCURRENCY)]
+    for t in threads:
+        t.start()
+    for t in threads:
+        t.join(timeout=30)
+
+    still_alive = [t for t in threads if t.is_alive()]
+    assert not still_alive, f"{len(still_alive)}/{CONCURRENCY} threads never finished"
+    assert not errors, f"unexpected exceptions acquiring the lock concurrently: {errors!r}"
+    assert entered == CONCURRENCY

TrungKiencding/AgentX-Workmate

(cherry picked from commit 739dc6d198a6cf76e776f3eae9fac3232ed1911c)

agent/credential_pool.py
@@ -967,6 +967,49 @@ class CredentialPool:
             logger.debug("Failed to sync from credentials file: %s", exc)
         return entry
 
+    def _sync_anthropic_entry_from_pool_store(
+        self, entry: PooledCredential
+    ) -> PooledCredential:
+        """Adopt an Anthropic token pair rotated by another pool instance.
+
+        Unlike ``_sync_anthropic_entry_from_credentials_file`` (which only
+        helps ``entry.source == "claude_code"`` by re-reading
+        ``~/.claude/.credentials.json``), this re-reads the exact persisted
+        row from the credential-pool store itself
+        (``~/.agentx/auth.json`` / profile equivalent), so it works for
+        every Anthropic source — ``claude_code``, ``hermes_pkce``, and
+        dashboard-issued ``manual:dashboard_pkce`` entries alike. Called
+        while the shared cross-process auth-store lock is held, mirroring
+        ``_sync_xai_oauth_entry_from_pool_store``.
+        """
+        if self.provider != "anthropic":
+            return entry
+        try:
+            persisted = next(
+                (
+                    payload
+                    for payload in read_credential_pool(self.provider)
+                    if isinstance(payload, dict) and payload.get("id") == entry.id
+                ),
+                None,
+            )
+            if not isinstance(persisted, dict):
+                return entry
+            stored = PooledCredential.from_dict(self.provider, persisted)
+            if (
+                stored.access_token != entry.access_token
+                or stored.refresh_token != entry.refresh_token
+            ):
+                logger.debug(
+                    "Pool entry %s: adopting Anthropic OAuth tokens rotated by another pool instance",
+                    entry.id,
+                )
+                self._replace_entry(entry, stored)
+                return stored
+        except Exception as exc:
+            logger.debug("Failed to sync Anthropic OAuth entry from credential pool: %s", exc)
+        return entry
+
     def _sync_codex_entry_from_auth_store(self, entry: PooledCredential) -> PooledCredential:
         """Sync a Codex device_code pool entry from auth.json if tokens differ.
 
@@ -1382,11 +1425,22 @@ class CredentialPool:
         # resolve_codex_runtime_credentials()).  When a waiter finally acquires
         # the lock, the in-lock re-sync below picks up the rotated token the
         # winner persisted and skips the POST.
-        if self.provider in ("openai-codex", "xai-oauth"):
+        # Anthropic's OAuth refresh tokens are single-use too (see
+        # agent/anthropic_adapter.py::_refresh_oauth_token), so the same
+        # cross-process serialization Codex/xAI get is required here.
+        # Previously "anthropic" was excluded from this tuple: two Hermes
+        # processes racing to refresh the same stale token would both POST,
+        # the loser got invalid_grant, and — for any source other than
+        # "claude_code" (hermes_pkce, dashboard-issued manual entries) —
+        # there was no recovery path at all, so the loser was marked
+        # exhausted despite a valid token existing on disk from the winner.
+        if self.provider in ("openai-codex", "xai-oauth", "anthropic"):
             sync_entry = (
                 self._sync_codex_entry_from_auth_store
                 if self.provider == "openai-codex"
                 else self._sync_xai_oauth_entry_from_pool_store
+                if self.provider == "xai-oauth"
+                else self._sync_anthropic_entry_from_pool_store
             )
             with _auth_store_lock(
                 timeout_seconds=self._single_use_refresh_lock_timeout()
@@ -1418,6 +1472,8 @@ class CredentialPool:
             "AGENTX_CODEX_REFRESH_TIMEOUT_SECONDS"
             if self.provider == "openai-codex"
             else "AGENTX_XAI_REFRESH_TIMEOUT_SECONDS"
+            if self.provider == "xai-oauth"
+            else "AGENTX_ANTHROPIC_REFRESH_TIMEOUT_SECONDS"
         )
         refresh_timeout_seconds = auth_mod.env_float(env_var, 20)
         return max(
@@ -1544,6 +1600,31 @@ class CredentialPool:
                     # Credentials file had a valid (non-expired) token — use it directly
                     logger.debug("Credentials file has valid token, using without refresh")
                     return synced
+            elif self.provider == "anthropic":
+                # Backstop for non-claude_code sources (hermes_pkce,
+                # manual:dashboard_pkce): the in-lock pre-check in
+                # _refresh_entry() should already have adopted a winner's
+                # rotated token before this POST was even attempted, but if
+                # the failure still happened (e.g. the winner persisted
+                # between our pre-check and our POST), re-read the pool
+                # store once more before giving up.
+                synced = self._sync_anthropic_entry_from_pool_store(entry)
+                if synced.refresh_token != entry.refresh_token:
+                    logger.debug(
+                        "Anthropic OAuth refresh failed but pool store has newer tokens — adopting"
+                    )
+                    updated = replace(
+                        synced,
+                        last_status=STATUS_OK,
+                        last_status_at=None,
+                        last_error_code=None,
+                        last_error_reason=None,
+                        last_error_message=None,
+                        last_error_reset_at=None,
+                    )
+                    self._replace_entry(synced, updated)
+                    self._persist()
+                    return updated
             # For xai-oauth: same race as nous — another process may have
             # consumed the refresh token between our proactive sync and the
             # HTTP call.  Re-check auth.json and adopt the fresh tokens if

tests/hermes_cli/test_auth_store_lock_concurrent.py
@@ -1,0 +1,79 @@
+"""Regression test for a Windows-only PermissionError under lock contention.
+
+``_file_lock()`` (backing ``_auth_store_lock()``) ensures its lock file has
+at least 1 byte of content before opening it for ``msvcrt.locking()`` (which
+requires a non-empty file). That "ensure content" write was unguarded: under
+real concurrency, one thread/process's ``msvcrt.locking()`` byte-range lock
+can already be held on the file at the exact moment another thread runs the
+same ensure-content check, and the write collides and raises
+``PermissionError`` -- uncaught, since it happens before the retry loop the
+rest of the function uses for exactly this kind of contention.
+
+This was found by a stress test for the Anthropic OAuth credential-pool fix
+(``tests/agent/test_anthropic_oauth_stress.py``), which reliably reproduces
+it (16/20 concurrent refreshes hit PermissionError pre-fix, deterministically
+across repeated runs) because the surrounding CredentialPool work widens the
+race window enough for real OS-level thread interleaving to land on it. That
+test is the authoritative regression guard for the exact failure mode. This
+file is deliberately a minimal, isolated stress test against
+``_auth_store_lock()`` directly -- best-effort coverage that does not always
+land on the same narrow window in isolation, but exercises the SAME shared
+primitive Codex/xAI/Nous already depend on for their single-use-refresh-token
+protection, so this bug was never Anthropic-specific.
+"""
+
+from __future__ import annotations
+
+import threading
+
+import pytest
+
+from hermes_cli.auth import _auth_store_lock
+
+CONCURRENCY = 40
+
+
+@pytest.fixture
+def hermes_home(tmp_path, monkeypatch):
+    monkeypatch.setenv("AGENTX_HOME", str(tmp_path))
+    return tmp_path
+
+
+def test_many_concurrent_lock_acquisitions_do_not_raise_permission_error(hermes_home):
+    """CONCURRENCY threads race to acquire/release the same auth-store lock.
+
+    None of them should ever see an uncaught PermissionError/OSError from
+    the lock-file "ensure content" pre-check -- only the intended
+    TimeoutError (never expected here, since each critical section is
+    instant) should be a possible failure mode.
+    """
+    errors: dict[int, BaseException] = {}
+    entered = 0
+    entered_lock = threading.Lock()
+    # Synchronize all threads to hit the lock-file "ensure content" pre-check
+    # at (as close as the OS scheduler allows to) the exact same instant --
+    # the race only exists on the very first acquisition against a fresh
+    # lock file, so a tight barrier maximizes the odds of reproducing it
+    # instead of relying on incidental thread-start jitter.
+    barrier = threading.Barrier(CONCURRENCY)
+
+    def _run(idx: int) -> None:
+        nonlocal entered
+        try:
+            barrier.wait(timeout=10)
+            with _auth_store_lock(timeout_seconds=10.0):
+                with entered_lock:
+                    entered += 1
+        except BaseException as exc:  # pragma: no cover - failure diagnostics
+            errors[idx] = exc
+
+    threads = [threading.Thread(target=_run, args=(i,)) for i in range(CONCURRENCY)]
+    for t in threads:
+        t.start()
+    for t in threads:
+        t.join(timeout=30)
+
+    still_alive = [t for t in threads if t.is_alive()]
+    assert not still_alive, f"{len(still_alive)}/{CONCURRENCY} threads never finished"
+    assert not errors, f"unexpected exceptions acquiring the lock concurrently: {errors!r}"
+    assert entered == CONCURRENCY