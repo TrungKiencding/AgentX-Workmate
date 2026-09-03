Carrie111998/Hermes

fix(mcp): restrict catalog environment writes

hermes_cli/config.py
@@ -215,6 +215,14 @@ _ENV_VAR_NAME_DENYLIST: frozenset[str] = frozenset({
     # NOT a HERMES_* blanket: integration credentials (HERMES_GEMINI_*,
     # HERMES_LANGFUSE_*, HERMES_SPOTIFY_*, ...) ARE allowed.
     "HERMES_HOME", "HERMES_PROFILE", "HERMES_CONFIG", "HERMES_ENV",
+    "HERMES_CONFIG_PATH", "HERMES_ENV_PATH",
+    # Hermes security policy / approval-routing context. These remain available
+    # through their dedicated CLI/config/session controls, but a generic
+    # credential writer must not persist them for the next process startup.
+    "HERMES_YOLO_MODE", "HERMES_ACCEPT_HOOKS", "HERMES_REDACT_SECRETS",
+    "HERMES_INTERACTIVE", "HERMES_EXEC_ASK", "HERMES_GATEWAY_SESSION",
+    "HERMES_CRON_SESSION", "HERMES_SINGLE_QUERY_SESSION",
+    "HERMES_SESSION_KEY", "HERMES_SESSION_PLATFORM",
 })
 
 
@@ -229,10 +237,22 @@ def _reject_denylisted_env_var(key: str) -> None:
             f"Environment variable {key!r} is on the writer denylist. "
             "Names that influence subprocess execution (LD_PRELOAD, "
             "PYTHONPATH, PATH, EDITOR, ...) or Hermes runtime location "
-            "(HERMES_HOME, HERMES_PROFILE, ...) cannot be persisted via "
+            "and security policy (HERMES_HOME, HERMES_YOLO_MODE, ...) "
+            "cannot be persisted via "
             "the env writer. If you really need this, edit "
             "~/.hermes/.env directly."
         )
+
+
+def validate_env_var_name_for_write(key: str) -> None:
+    """Validate an environment name before a generic persistence write.
+
+    Exposed separately from :func:`save_env_value` so batch-style callers can
+    validate their complete request before writing the first value.
+    """
+    if not _ENV_VAR_NAME_RE.match(key):
+        raise ValueError(f"Invalid environment variable name: {key!r}")
+    _reject_denylisted_env_var(key)
 
 _LAST_EXPANDED_CONFIG_BY_PATH: Dict[str, Any] = {}
 # (path, mtime_ns, size) -> cached expanded config dict.
@@ -4222,9 +4242,7 @@ def save_env_value(key: str, value: str):
             file=sys.stderr,
         )
         return
-    if not _ENV_VAR_NAME_RE.match(key):
-        raise ValueError(f"Invalid environment variable name: {key!r}")
-    _reject_denylisted_env_var(key)
+    validate_env_var_name_for_write(key)
     value = value.replace("\n", "").replace("\r", "")
     # API keys / tokens must be ASCII — strip non-ASCII with a warning.
     value = _check_non_ascii_credential(key, value)

tests/hermes_cli/test_mcp_catalog_env_boundary.py
@@ -1,0 +1,181 @@
+"""Security boundary tests for dashboard MCP catalog credential writes."""
+
+from __future__ import annotations
+
+import os
+from pathlib import Path
+
+import pytest
+import yaml
+from fastapi.testclient import TestClient
+
+from hermes_cli.web_server import _SESSION_TOKEN, app
+
+
+HEADERS = {"X-Hermes-Session-Token": _SESSION_TOKEN}
+
+
+@pytest.fixture
+def catalog_env(tmp_path: Path, monkeypatch: pytest.MonkeyPatch, _isolate_hermes_home):
+    """Install one synthetic API-key catalog entry in the isolated test home."""
+    from hermes_constants import get_hermes_home
+    from hermes_cli.config import invalidate_env_cache
+
+    catalog = tmp_path / "optional-mcps"
+    entry_dir = catalog / "demo"
+    entry_dir.mkdir(parents=True)
+    (entry_dir / "manifest.yaml").write_text(
+        yaml.safe_dump(
+            {
+                "manifest_version": 1,
+                "name": "demo",
+                "description": "Synthetic dashboard boundary fixture",
+                "source": "https://example.test/demo",
+                "transport": {
+                    "type": "stdio",
+                    "command": "demo-mcp",
+                },
+                "auth": {
+                    "type": "api_key",
+                    "env": [
+                        {
+                            "name": "DEMO_API_KEY",
+                            "prompt": "Demo API key",
+                            "secret": True,
+                        }
+                    ],
+                },
+            }
+        ),
+        encoding="utf-8",
+    )
+    monkeypatch.setenv("HERMES_OPTIONAL_MCPS", str(catalog))
+    invalidate_env_cache()
+    return get_hermes_home()
+
+
+@pytest.fixture
+def client():
+    with TestClient(app) as test_client:
+        yield test_client
+
+
+def test_catalog_rejects_undeclared_key_before_any_write_or_install(
+    client: TestClient,
+    catalog_env: Path,
+    monkeypatch: pytest.MonkeyPatch,
+):
+    import hermes_cli.mcp_catalog as mcp_catalog
+
+    installs: list[str] = []
+    monkeypatch.setattr(
+        mcp_catalog,
+        "install_entry",
+        lambda entry, enable=True: installs.append(entry.name),
+    )
+
+    response = client.post(
+        "/api/mcp/catalog/install",
+        headers=HEADERS,
+        json={
+            "name": "demo",
+            "env": {
+                "DEMO_API_KEY": "valid-demo-value",
+                "UNRELATED_SETTING": "must-not-land",
+            },
+        },
+    )
+
+    assert response.status_code == 400
+    detail = response.json()["detail"]
+    assert "UNRELATED_SETTING" in detail
+    assert "valid-demo-value" not in detail
+    assert "must-not-land" not in detail
+    assert installs == []
+    env_path = catalog_env / ".env"
+    assert not env_path.exists() or env_path.read_text(encoding="utf-8") == ""
+
+
+def test_catalog_cannot_declare_reserved_control_key(
+    client: TestClient,
+    catalog_env: Path,
+    monkeypatch: pytest.MonkeyPatch,
+):
+    import hermes_cli.mcp_catalog as mcp_catalog
+
+    catalog_root = Path(os.environ["HERMES_OPTIONAL_MCPS"])
+    manifest_path = catalog_root / "demo" / "manifest.yaml"
+    manifest = yaml.safe_load(manifest_path.read_text(encoding="utf-8"))
+    manifest["auth"]["env"].append(
+        {
+            "name": "HERMES_YOLO_MODE",
+            "prompt": "Unsafe control",
+            "secret": False,
+        }
+    )
+    manifest_path.write_text(yaml.safe_dump(manifest), encoding="utf-8")
+
+    installs: list[str] = []
+    monkeypatch.setattr(
+        mcp_catalog,
+        "install_entry",
+        lambda entry, enable=True: installs.append(entry.name),
+    )
+
+    response = client.post(
+        "/api/mcp/catalog/install",
+        headers=HEADERS,
+        json={"name": "demo", "env": {"HERMES_YOLO_MODE": "1"}},
+    )
+
+    assert response.status_code == 400
+    assert "denylist" in response.json()["detail"]
+    assert installs == []
+    env_path = catalog_env / ".env"
+    assert not env_path.exists() or "HERMES_YOLO_MODE" not in env_path.read_text(
+        encoding="utf-8"
+    )
+
+
+def test_catalog_accepts_declared_credential(
+    client: TestClient,
+    catalog_env: Path,
+    monkeypatch: pytest.MonkeyPatch,
+):
+    import hermes_cli.mcp_catalog as mcp_catalog
+
+    installs: list[str] = []
+    monkeypatch.setattr(
+        mcp_catalog,
+        "install_entry",
+        lambda entry, enable=True: installs.append(entry.name),
+    )
+
+    response = client.post(
+        "/api/mcp/catalog/install",
+        headers=HEADERS,
+        json={"name": "demo", "env": {"DEMO_API_KEY": "valid-demo-value"}},
+    )
+
+    assert response.status_code == 200
+    assert installs == ["demo"]
+    assert "DEMO_API_KEY=valid-demo-value" in (
+        catalog_env / ".env"
+    ).read_text(encoding="utf-8")
+
+
+def test_generic_env_endpoint_rejects_yolo_control_key(
+    client: TestClient,
+    catalog_env: Path,
+):
+    response = client.put(
+        "/api/env",
+        headers=HEADERS,
+        json={"key": "HERMES_YOLO_MODE", "value": "1"},
+    )
+
+    assert response.status_code == 400
+    env_path = catalog_env / ".env"
+    assert not env_path.exists() or "HERMES_YOLO_MODE" not in env_path.read_text(
+        encoding="utf-8"
+    )

TrungKiencding/AgentX-Workmate

(cherry picked from commit 08cf4fea5d76afd63365b2aa84bc2aa098d556ec)

hermes_cli/config.py
@@ -215,6 +215,14 @@ _ENV_VAR_NAME_DENYLIST: frozenset[str] = frozenset({
     # NOT a AGENTX_* blanket: integration credentials (AGENTX_GEMINI_*,
     # AGENTX_LANGFUSE_*, AGENTX_SPOTIFY_*, ...) ARE allowed.
     "AGENTX_HOME", "AGENTX_PROFILE", "AGENTX_CONFIG", "AGENTX_ENV",
+    "AGENTX_CONFIG_PATH", "AGENTX_ENV_PATH",
+    # AgentX security policy / approval-routing context. These remain available
+    # through their dedicated CLI/config/session controls, but a generic
+    # credential writer must not persist them for the next process startup.
+    "AGENTX_YOLO_MODE", "AGENTX_ACCEPT_HOOKS", "AGENTX_REDACT_SECRETS",
+    "AGENTX_INTERACTIVE", "AGENTX_EXEC_ASK", "AGENTX_GATEWAY_SESSION",
+    "AGENTX_CRON_SESSION", "AGENTX_SINGLE_QUERY_SESSION",
+    "AGENTX_SESSION_KEY", "AGENTX_SESSION_PLATFORM",
 })
 
 
@@ -229,10 +237,22 @@ def _reject_denylisted_env_var(key: str) -> None:
             f"Environment variable {key!r} is on the writer denylist. "
             "Names that influence subprocess execution (LD_PRELOAD, "
             "PYTHONPATH, PATH, EDITOR, ...) or AgentX runtime location "
-            "(AGENTX_HOME, AGENTX_PROFILE, ...) cannot be persisted via "
+            "and security policy (AGENTX_HOME, AGENTX_YOLO_MODE, ...) "
+            "cannot be persisted via "
             "the env writer. If you really need this, edit "
             "~/.agentx/.env directly."
         )
+
+
+def validate_env_var_name_for_write(key: str) -> None:
+    """Validate an environment name before a generic persistence write.
+
+    Exposed separately from :func:`save_env_value` so batch-style callers can
+    validate their complete request before writing the first value.
+    """
+    if not _ENV_VAR_NAME_RE.match(key):
+        raise ValueError(f"Invalid environment variable name: {key!r}")
+    _reject_denylisted_env_var(key)
 
 _LAST_EXPANDED_CONFIG_BY_PATH: Dict[str, Any] = {}
 # (path, mtime_ns, size) -> cached expanded config dict.
@@ -4042,9 +4062,7 @@ def save_env_value(key: str, value: str):
             file=sys.stderr,
         )
         return
-    if not _ENV_VAR_NAME_RE.match(key):
-        raise ValueError(f"Invalid environment variable name: {key!r}")
-    _reject_denylisted_env_var(key)
+    validate_env_var_name_for_write(key)
     value = value.replace("\n", "").replace("\r", "")
     # API keys / tokens must be ASCII — strip non-ASCII with a warning.
     value = _check_non_ascii_credential(key, value)

tests/hermes_cli/test_mcp_catalog_env_boundary.py
@@ -1,0 +1,189 @@
+"""Security boundary tests for dashboard MCP catalog credential writes."""
+
+from __future__ import annotations
+
+import os
+from pathlib import Path
+
+import pytest
+import yaml
+from fastapi.testclient import TestClient
+
+from hermes_cli.web_server import _SESSION_TOKEN, app
+
+
+HEADERS = {"X-AgentX-Session-Token": _SESSION_TOKEN}
+
+
+@pytest.fixture
+def catalog_env(tmp_path: Path, monkeypatch: pytest.MonkeyPatch, _isolate_hermes_home):
+    """Install one synthetic API-key catalog entry in the isolated test home."""
+    from hermes_constants import get_hermes_home
+    from hermes_cli.config import invalidate_env_cache
+
+    catalog = tmp_path / "optional-mcps"
+    entry_dir = catalog / "demo"
+    entry_dir.mkdir(parents=True)
+    (entry_dir / "manifest.yaml").write_text(
+        yaml.safe_dump(
+            {
+                "manifest_version": 1,
+                "name": "demo",
+                "description": "Synthetic dashboard boundary fixture",
+                "source": "https://example.test/demo",
+                "transport": {
+                    "type": "stdio",
+                    "command": "demo-mcp",
+                },
+                "auth": {
+                    "type": "api_key",
+                    "env": [
+                        {
+                            "name": "DEMO_API_KEY",
+                            "prompt": "Demo API key",
+                            "secret": True,
+                        }
+                    ],
+                },
+            }
+        ),
+        encoding="utf-8",
+    )
+    monkeypatch.setenv("AGENTX_OPTIONAL_MCPS", str(catalog))
+    invalidate_env_cache()
+    return get_hermes_home()
+
+
+@pytest.fixture
+def client(monkeypatch: pytest.MonkeyPatch):
+    # The dashboard rejects unauthenticated /api/ calls; attach the
+    # process-local session token like the rest of the web-server tests do,
+    # and pin the loopback (token) mode so a gate test that ran earlier in
+    # the same session cannot leave the app in cookie-gated mode.
+    from hermes_cli.web_server import _SESSION_HEADER_NAME, _SESSION_TOKEN
+
+    monkeypatch.setattr(app.state, "auth_required", False, raising=False)
+    with TestClient(app) as test_client:
+        test_client.headers[_SESSION_HEADER_NAME] = _SESSION_TOKEN
+        yield test_client
+
+
+def test_catalog_rejects_undeclared_key_before_any_write_or_install(
+    client: TestClient,
+    catalog_env: Path,
+    monkeypatch: pytest.MonkeyPatch,
+):
+    import hermes_cli.mcp_catalog as mcp_catalog
+
+    installs: list[str] = []
+    monkeypatch.setattr(
+        mcp_catalog,
+        "install_entry",
+        lambda entry, enable=True: installs.append(entry.name),
+    )
+
+    response = client.post(
+        "/api/mcp/catalog/install",
+        headers=HEADERS,
+        json={
+            "name": "demo",
+            "env": {
+                "DEMO_API_KEY": "valid-demo-value",
+                "UNRELATED_SETTING": "must-not-land",
+            },
+        },
+    )
+
+    assert response.status_code == 400
+    detail = response.json()["detail"]
+    assert "UNRELATED_SETTING" in detail
+    assert "valid-demo-value" not in detail
+    assert "must-not-land" not in detail
+    assert installs == []
+    env_path = catalog_env / ".env"
+    assert not env_path.exists() or env_path.read_text(encoding="utf-8") == ""
+
+
+def test_catalog_cannot_declare_reserved_control_key(
+    client: TestClient,
+    catalog_env: Path,
+    monkeypatch: pytest.MonkeyPatch,
+):
+    import hermes_cli.mcp_catalog as mcp_catalog
+
+    catalog_root = Path(os.environ["AGENTX_OPTIONAL_MCPS"])
+    manifest_path = catalog_root / "demo" / "manifest.yaml"
+    manifest = yaml.safe_load(manifest_path.read_text(encoding="utf-8"))
+    manifest["auth"]["env"].append(
+        {
+            "name": "AGENTX_YOLO_MODE",
+            "prompt": "Unsafe control",
+            "secret": False,
+        }
+    )
+    manifest_path.write_text(yaml.safe_dump(manifest), encoding="utf-8")
+
+    installs: list[str] = []
+    monkeypatch.setattr(
+        mcp_catalog,
+        "install_entry",
+        lambda entry, enable=True: installs.append(entry.name),
+    )
+
+    response = client.post(
+        "/api/mcp/catalog/install",
+        headers=HEADERS,
+        json={"name": "demo", "env": {"AGENTX_YOLO_MODE": "1"}},
+    )
+
+    assert response.status_code == 400
+    assert "denylist" in response.json()["detail"]
+    assert installs == []
+    env_path = catalog_env / ".env"
+    assert not env_path.exists() or "AGENTX_YOLO_MODE" not in env_path.read_text(
+        encoding="utf-8"
+    )
+
+
+def test_catalog_accepts_declared_credential(
+    client: TestClient,
+    catalog_env: Path,
+    monkeypatch: pytest.MonkeyPatch,
+):
+    import hermes_cli.mcp_catalog as mcp_catalog
+
+    installs: list[str] = []
+    monkeypatch.setattr(
+        mcp_catalog,
+        "install_entry",
+        lambda entry, enable=True: installs.append(entry.name),
+    )
+
+    response = client.post(
+        "/api/mcp/catalog/install",
+        headers=HEADERS,
+        json={"name": "demo", "env": {"DEMO_API_KEY": "valid-demo-value"}},
+    )
+
+    assert response.status_code == 200
+    assert installs == ["demo"]
+    assert "DEMO_API_KEY=valid-demo-value" in (
+        catalog_env / ".env"
+    ).read_text(encoding="utf-8")
+
+
+def test_generic_env_endpoint_rejects_yolo_control_key(
+    client: TestClient,
+    catalog_env: Path,
+):
+    response = client.put(
+        "/api/env",
+        headers=HEADERS,
+        json={"key": "AGENTX_YOLO_MODE", "value": "1"},
+    )
+
+    assert response.status_code == 400
+    env_path = catalog_env / ".env"
+    assert not env_path.exists() or "AGENTX_YOLO_MODE" not in env_path.read_text(
+        encoding="utf-8"
+    )